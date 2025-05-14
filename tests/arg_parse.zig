c03a7923782b177561961c927812d81659f1d5aa
brew02 <brodiea2005@gmail.com>
2025-05-13 19:10:46 -0600

    Finish unnamed argument tests

--- tests/arg_parse.zig
+++ tests/arg_parse.zig
@@ -10,6 +10,21 @@ pub fn ZYAN_MAKE_STATUS(comptime err: u32, comptime module: u32, comptime code: 
     return ((err & 1) << 31) | ((module & 0x7FF) << 20) | (code & 0xFFFFF);
 }
 
+fn cvtStringView(sv: *const c.ZyanStringView) ?[:0]const u8 {
+    var buf: ?[*:0]const u8 = null;
+    if (c.ZYAN_FAILED(c.ZyanStringViewGetData(sv, @ptrCast(&buf))) == 1) return null;
+    var len: c.ZyanUSize = 0;
+    if (c.ZYAN_FAILED(c.ZyanStringViewGetSize(sv, &len)) == 1) return null;
+
+    if (buf) |val| {
+        const ret = std.mem.span(val);
+        if (ret.len != len) return null;
+        return ret;
+    } else {
+        return null;
+    }
+}
+
 fn unnamedArgTest(min: c.ZyanUSize, max: c.ZyanUSize) struct {
     c.ZyanStatus,
     c.ZyanVector,
@@ -48,4 +63,30 @@ test "too many unnamed args" {
 
     try std.testing.expectEqual(ZYAN_MAKE_STATUS(@as(c_uint, 1), c.ZYAN_MODULE_ARGPARSE, @as(c_uint, 0x02)), status);
     try std.testing.expectEqualStrings("xxx", std.mem.span(err_tok.?));
+}
+
+test "perfect fit unnamed args" {
+    const status, const parsed, _ = unnamedArgTest(2, 2);
+
+    try std.testing.expect(c.ZYAN_SUCCESS(status));
+    var size: c.ZyanUSize = 0;
+
+    try std.testing.expect(c.ZYAN_SUCCESS(c.ZyanVectorGetSize(&parsed, &size)));
+    try std.testing.expectEqual(2, size);
+
+    var arg: ?*const c.ZyanArgParseArg = @alignCast(@ptrCast(c.ZyanVectorGet(&parsed, 0)));
+    try std.testing.expect(arg != null);
+    try std.testing.expect(arg.?.has_value == 1);
+
+    var val = cvtStringView(&arg.?.value);
+    try std.testing.expect(val != null);
+    try std.testing.expectEqualStrings("a", val.?);
+
+    arg = @alignCast(@ptrCast(c.ZyanVectorGet(&parsed, 1)));
+    try std.testing.expect(arg != null);
+    try std.testing.expect(arg.?.has_value == 1);
+
+    val = cvtStringView(&arg.?.value);
+    try std.testing.expect(val != null);
+    try std.testing.expectEqualStrings("xxx", val.?);
 }
